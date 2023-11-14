1e8b754d69536ef24588e418bbd4cfc70cd12106
nicolas.fracchia <nicolas.fracchia@freshwatercreative.ca>
2023-11-13 18:01:02 -0700

    Assesment finished

---
+++ miniproject1.ts
@@ -0,0 +1,39 @@
+class Students {
+    name: String;
+    age: Number;
+    gender: String = "Female";
+    nationality: String;
+
+    constructor(name:String, age:Number, nationality:String){
+        this.name = name;
+        this.age = age;
+        this.nationality = nationality;
+    }
+
+    getNationality(): void {
+        console.log(this.nationality);
+    }
+}
+
+class UnderGraduates extends Students implements Iundergrad{
+    batch: Number;
+    gpa: Number;
+
+    constructor(batch:Number, name:String, age:Number, nationality:String){
+        super(name, age, nationality);
+        this.batch = batch;
+    }
+
+}
+
+interface Iundergrad{
+    name: String;
+    age: Number;
+    gender: String;
+    nationality: String;
+    batch: Number;
+    gpa: Number;
+}
+
+const ug: UnderGraduates = new UnderGraduates(123456, "Nicolas Fracchia", 32, "Argentinian");
+ug.getNationality();
